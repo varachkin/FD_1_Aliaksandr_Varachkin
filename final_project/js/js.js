56c168900f49ad019009dcdd0b438678d7c404c1
unknown <varachkin@gmail.com>
2020-10-06 21:05:53 +0300

    working

--- final_project/js/js.js
+++ final_project/js/js.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "200px";
-    document.getElementById("main").style.marginRight = "200px";
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("main").style.marginRight = "100%";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
